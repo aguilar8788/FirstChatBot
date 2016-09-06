612aa96db6c2c9e7528243124d3b4b8610ed86ca
Peter Aguilar <peter.aguilar2287@gmail.com>
2016-09-06 16:33:18 -0600

    add content bubble

--- index.js
+++ index.js
@@ -106,7 +106,16 @@ app.post('/webhook/', function (req, res) {
     let sender = event.sender.id;
     if(event.message && event.message.text) {
       let text = event.message.text;
+      if (text == 'Generic') {
+        sendGenericMessage(sender);
+        continue;
+      }
       sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200));
+    }
+    if (event.postback) {
+      let text = JSON.stringify(event.postback);
+      sendTextMessage(sender, "Postback received: " + text.substring(0, 200), token);
+      continue;
     }
   }
   res.sendStatus(200);
