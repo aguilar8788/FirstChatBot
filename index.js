662aeb0189d93e7f32ffcaf3ce3023b6040aa514
Peter Aguilar <peter.aguilar2287@gmail.com>
2016-09-06 16:37:25 -0600

    testing alert

--- index.js
+++ index.js
@@ -106,6 +106,7 @@ app.post('/webhook/', function (req, res) {
     let sender = event.sender.id;
     if(event.message && event.message.text) {
       let text = event.message.text;
+      alert(text);
       if (text == 'Generic') {
         sendGenericMessage(sender);
         continue;
