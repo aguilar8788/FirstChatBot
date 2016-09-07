1d742d390110126882350ab7a4334e2e3c0f1162
Peter Aguilar <peter.aguilar2287@gmail.com>
2016-09-07 11:46:58 -0600

    testing troubleshooting

--- index.js
+++ index.js
@@ -29,30 +29,20 @@ function sendGenericMessage(sender) {
         "attachment": {
             "type": "template",
             "payload": {
-                "template_type": "generic",
+                "template_type": "Computer",
                 "elements": [{
-                    "title": "First card",
-                    "subtitle": "Element #1 of an hscroll",
+                    "title": "Common Computer Issues",
+                    "subtitle": "What issue are you having?",
                     "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
                     "buttons": [{
-                        "type": "web_url",
-                        "url": "https://www.messenger.com",
-                        "title": "web url"
+                        "type": "Wont Turn On",
+                        "title": "Wont Turn On"
                     }, {
-                        "type": "postback",
-                        "title": "Postback",
+                        "type": "No internet connection",
+                        "title": "No internet connection",
                         "payload": "Payload for first element in a generic bubble",
                     }],
-                }, {
-                    "title": "Second card",
-                    "subtitle": "Element #2 of an hscroll",
-                    "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
-                    "buttons": [{
-                        "type": "postback",
-                        "title": "Postback",
-                        "payload": "Payload for second element in a generic bubble",
-                    }],
-                }]
+                }
             }
         }
     }
