91aeff500c21fc8e7d51dd0dceaf44e79ebb7847
Peter Aguilar <peter.aguilar2287@gmail.com>
2016-09-07 11:49:30 -0600

    fix server error

--- index.js
+++ index.js
@@ -42,7 +42,16 @@ function sendGenericMessage(sender) {
                         "title": "No internet connection",
                         "payload": "Payload for first element in a generic bubble",
                     }],
-                }
+                }, {
+                    "title": "Second card",
+                    "subtitle": "Element #2 of an hscroll",
+                    "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
+                    "buttons": [{
+                        "type": "postback",
+                        "title": "Postback",
+                        "payload": "Payload for second element in a generic bubble",
+                    }],
+                }]
             }
         }
     }
