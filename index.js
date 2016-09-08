99613c0cceb4224fe01c00a5b25dd24d54e85f2e
Peter Aguilar <peter.aguilar2287@gmail.com>
2016-09-08 14:45:34 -0600

    make changes

--- index.js
+++ index.js
@@ -253,7 +253,7 @@ app.post('/webhook/', function (req, res) {
       let text = JSON.stringify(event.postback);
       var response = event.postback.payload;
       if(response == "Computer"){
-        computer.send();
+        computer.send(sender);
         continue;
       }
       else if (response == "noBoot") {
