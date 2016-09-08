ba07663f73c2368ebe515f4646eb392b47b34519
Peter Aguilar <peter.aguilar2287@gmail.com>
2016-09-08 14:27:35 -0600

    make changes

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 'use strict'
-var computer = require("/computerTroubleshooting");
+var computer = require("./computerTroubleshooting");
 const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
