605f67c10b340fdbeb4c9bdcf4becd6d7c72631e
Peter Aguilar <peter.aguilar2287@gmail.com>
2016-09-08 13:24:33 -0600

    make changes

--- index.js
+++ index.js
@@ -246,7 +246,7 @@ app.post('/webhook/', function (req, res) {
     }else if (event.postback) {
       var userChoice = event.postback.payload;
 
-      var troubleshooting = ["First lets try the basics. Please insure that your computer has power, or if it is a laptop insure your battery is charged.", "Please hold the power button down for 30 seconds, release the button, then finally try to turn the computer back on."]
+      var troubleshooting = ["First lets try the basics. Please insure that your computer has power, or if it is a laptop insure your battery is charged.", "Next we should check if there are any lights on. This will indicate that we have power, which would mean there is an issue with the display.", "Next, I want you to hold the power button down for 30 seconds, release the button, then finally try to turn the computer back on."]
       let text = JSON.stringify(event.postback);
       var response = event.postback.payload;
       if(response == "Computer"){
@@ -256,12 +256,14 @@ app.post('/webhook/', function (req, res) {
       else if (response == "noBoot") {
         setTimeout(function() {sendTextMessage(sender, "Hmmm, well lets figure this out together.");}, 3000);
         setTimeout(function() {sendTextMessage(sender, troubleshooting[0])}, 6000);
+        troubleshooting.shift();
         setTimeout(function() {confirmation(sender);}, 9000);
         continue;
       }
       else if(userChoice == "yes") {
         setTimeout(function() {sendTextMessage(sender, "Good, lets move on...");}, 2000);
-        setTimeout(function() {sendTextMessage(sender, troubleshooting[counter])}, 6000);
+        setTimeout(function() {sendTextMessage(sender, troubleshooting[0])}, 6000);
+        setTimeout(function() {confirmation(sender);}, 9000);
 
         continue;
       }else if(userChoice == "no") {
