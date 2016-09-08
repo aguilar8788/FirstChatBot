ec717f3b3faa63eb35446ae8fcf49dd5d30d4711
Peter Aguilar <peter.aguilar2287@gmail.com>
2016-09-08 11:49:55 -0600

    add logic

--- index.js
+++ index.js
@@ -245,8 +245,8 @@ app.post('/webhook/', function (req, res) {
         continue;
       }
       else if (response == "noBoot") {
-        setTimeout(function() {sendTextMessage(sender, "I'm sorry you are having this issue. Let's try to get your computer up and running again.");}, 3000);
-        setTimeout(function() {sendTextMessage(sender, "First lets try the basics. Please insure that your computer has power or if it is a laptop insure your battery is charged.")}, 6000);
+        setTimeout(function() {sendTextMessage(sender, "Hmmm, well lets figure this out together.");}, 3000);
+        setTimeout(function() {sendTextMessage(sender, "First lets try the basics. Please insure that your computer has power, or if it is a laptop insure your battery is charged.")}, 6000);
         setTimeout(function() {confirmation(sender);}, 9000);
         continue;
       }
