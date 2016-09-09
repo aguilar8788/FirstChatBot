c5bd1412b5dea4ee5bb9ecf5906b364da5eabfb0
Peter Aguilar <peter.aguilar2287@gmail.com>
2016-09-09 14:28:59 -0600

    make changes

--- index.js
+++ index.js
@@ -131,7 +131,7 @@ app.post('/webhook/', function (req, res) {
       let text = JSON.stringify(event.postback);
       var response = event.postback.payload;
 
-      if(response == "cpNoBoot" || response == "yes"){
+      if(response == "cpNoBoot" || response == "cpYes"){
         setTimeout(function() {message.sendTextMessage(sender, computer.computerWillNotBoot[0]);}, 6000);
         setTimeout(function() {computer.compConfirmation(sender);}, 9000);
         computer.computerWillNotBoot.shift()
