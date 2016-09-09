ed37ebd1baaa39aca89ac69c4e1010748e952fc0
Peter Aguilar <peter.aguilar2287@gmail.com>
2016-09-09 11:09:08 -0600

    make changes

--- index.js
+++ index.js
@@ -145,7 +145,7 @@ app.post('/webhook/', function (req, res) {
     let sender = event.sender.id;
     if(event.message && event.message.text) {
       let text = event.message.text;
-      var makeResponseArray = response.split(' ');
+      var makeResponseArray = text.split(' ');
       console.log(makeResponseArray);
       setTimeout(function() {message.sendTextMessage(sender, "Hello, What can I troubleshoot for you today?");}, 1000);
       setTimeout(function() {computer.sendGenericMessage(sender); }, 2000);
