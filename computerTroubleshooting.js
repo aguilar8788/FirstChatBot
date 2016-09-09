8d9dc8fc6a369cad10a4a45a7f4dae9ac0af5409
Peter Aguilar <peter.aguilar2287@gmail.com>
2016-09-09 14:17:00 -0600

    make changes

--- computerTroubleshooting.js
+++ computerTroubleshooting.js
@@ -89,10 +89,8 @@ module.exports = {
   computerVirus: ["First open a new web browser, and download a program called malwarebytes, which can be found at https://www.malwarebytes.com/antimalware/. THis is the virus software I use.",
                   "Next, run the malwarbytes program, and go through the prompts to install it.", "Once the program is installed, and it is running, click the scan button. This video is helpful if you are lost https://www.youtube.com/watch?v=RjrAceE-W4E",
                   "When the program finishes it will display any threats that it found. It will ask to quarantine the threats. Click yes to confirm.",
-                  "Finally, restart your computer to see if it eliminated your virus issue."],
-  cpNoBootLogic: function(userinput) {
-  
-  }
+                  "Finally, restart your computer to see if it eliminated your virus issue."]
+
 }
 
 
