8c2ef59c4592acecce8be77907ddaf52c5f090fb
Peter Aguilar <peter.aguilar2287@gmail.com>
2016-09-16 13:49:47 -0600

    make changes

--- index.js
+++ index.js
@@ -61,61 +61,60 @@ app.post('/webhook/', function (req, res) {
           continue;
         }
       }
-    }else if (event.postback) {
-      var userChoice = event.postback.payload;
-      let text = JSON.stringify(event.postback);
-      var response = event.postback.payload;
+      }else if (event.postback) {
+        var userChoice = event.postback.payload;
+        let text = JSON.stringify(event.postback);
+        var response = event.postback.payload;
 
-      if(response == "cpNoBoot" || response == "cpYes"){
-        if(computer.computerWillNotBoot[computer.counter] == "end"){
-          setTimeout(function() {message.sendTextMessage(sender, computer.confirmNoBootFixed(sender));}, 3000);
-          computer.counter = 0;
-        }else{
-          setTimeout(function() {message.sendTextMessage(sender, computer.computerWillNotBoot[computer.counter]);}, 3000);
-          setTimeout(function() {computer.compConfirmation(sender);}, 6000);
-          setTimeout(function() {computer.counter++;}, 4000);
-          continue;
-        }
+        if(response == "cpNoBoot" || response == "cpYes"){
+          if(computer.computerWillNotBoot[computer.counter] == "end"){
+            setTimeout(function() {message.sendTextMessage(sender, computer.confirmNoBootFixed(sender));}, 3000);
+            computer.counter = 0;
+          }else{
+            setTimeout(function() {message.sendTextMessage(sender, computer.computerWillNotBoot[computer.counter]);}, 3000);
+            setTimeout(function() {computer.compConfirmation(sender);}, 6000);
+            setTimeout(function() {computer.counter++;}, 4000);
+            continue;
+          }
 
-      }else if(response == "cpNoNetwork" || response == "cpNoIntYes"){
-        if(computer.computerNoInternet[computer.counter] == "end"){
-          setTimeout(function() {message.sendTextMessage(sender, computer.confirmNetworkFixed(sender));}, 3000);
-          computer.counter = 0;
-        }else {
-          setTimeout(function() {message.sendTextMessage(sender, computer.computerNoInternet[computer.counter]);}, 3000);
-          setTimeout(function() {computer.compNoIntConfirmation(sender);}, 6000);
-          setTimeout(function() {computer.counter++;}, 4000);
-          continue;
-        }
-      }else if(response == "cpvirus" || response == "cpVirusYes"){
-        if(computer.computerVirus[computer.counter] == "end"){
-          setTimeout(function() {message.sendTextMessage(sender, confirmVirusFixed(sender));}, 3000);
-          computer.counter = 0;
-        }else {
-          setTimeout(function() {message.sendTextMessage(sender, computer.computerVirus[computer.counter]);}, 3000);
-          setTimeout(function() {computer.compVirusConfirmation(sender);}, 6000);
-          setTimeout(function() {computer.counter++;}, 4000);
-          continue;
-        }
-      }else if (response == "bootfixNo"){
-        setTimeout(function() {message.sendTextMessage(sender, "Well sorry this was not helpful.")}, 3000);
-        setTimeout(function() {message.sendTextMessage(sender, "Here are some resources from the internet that may help. \n http://www.pcadvisor.co.uk/how-to/laptop/how-fix-pc-that-wont-boot-3528959/ \n" +
-                                                                "http://www.howtogeek.com/173828/what-to-do-when-windows-wont-boot/ \n" + "http://www.macworld.co.uk/how-to/mac/10-steps-take-when-your-mac-wont-start-up-or-turn-on-3423817/")}, 4000);
-        setTimeout(function() {message.sendTextMessage(sender, "If you would like help with a new issue just ask. Have a good day.")}, 5000);
-      }else if (response == "intYes"){
-        if(computer.computerVirus[computer.counter] == "end"){
-          setTimeout(function() {message.sendTextMessage(sender, internet.confirmInternetFixed(sender));}, 3000);
-          computer.counter = 0;
-        }else {
-          setTimeout(function() {message.sendTextMessage(sender, internet.internetTroubleshooting[computer.counter]);}, 3000);
-          setTimeout(function() {internet.intConfirmation(sender);}, 6000);
-          setTimeout(function() {computer.counter++;}, 4000);
-          continue;
+        }else if(response == "cpNoNetwork" || response == "cpNoIntYes"){
+          if(computer.computerNoInternet[computer.counter] == "end"){
+            setTimeout(function() {message.sendTextMessage(sender, computer.confirmNetworkFixed(sender));}, 3000);
+            computer.counter = 0;
+          }else {
+            setTimeout(function() {message.sendTextMessage(sender, computer.computerNoInternet[computer.counter]);}, 3000);
+            setTimeout(function() {computer.compNoIntConfirmation(sender);}, 6000);
+            setTimeout(function() {computer.counter++;}, 4000);
+            continue;
+          }
+        }else if(response == "cpvirus" || response == "cpVirusYes"){
+          if(computer.computerVirus[computer.counter] == "end"){
+            setTimeout(function() {message.sendTextMessage(sender, confirmVirusFixed(sender));}, 3000);
+            computer.counter = 0;
+          }else {
+            setTimeout(function() {message.sendTextMessage(sender, computer.computerVirus[computer.counter]);}, 3000);
+            setTimeout(function() {computer.compVirusConfirmation(sender);}, 6000);
+            setTimeout(function() {computer.counter++;}, 4000);
+            continue;
+          }
+        }else if (response == "bootfixNo"){
+          setTimeout(function() {message.sendTextMessage(sender, "Well sorry this was not helpful.")}, 3000);
+          setTimeout(function() {message.sendTextMessage(sender, "Here are some resources from the internet that may help. \n http://www.pcadvisor.co.uk/how-to/laptop/how-fix-pc-that-wont-boot-3528959/ \n" +
+                                                                  "http://www.howtogeek.com/173828/what-to-do-when-windows-wont-boot/ \n" + "http://www.macworld.co.uk/how-to/mac/10-steps-take-when-your-mac-wont-start-up-or-turn-on-3423817/")}, 4000);
+          setTimeout(function() {message.sendTextMessage(sender, "If you would like help with a new issue just ask. Have a good day.")}, 5000);
+        }else if (response == "intYes"){
+          if(computer.computerVirus[computer.counter] == "end"){
+            setTimeout(function() {message.sendTextMessage(sender, internet.confirmInternetFixed(sender));}, 3000);
+            computer.counter = 0;
+          }else {
+            setTimeout(function() {message.sendTextMessage(sender, internet.internetTroubleshooting[computer.counter]);}, 3000);
+            setTimeout(function() {internet.intConfirmation(sender);}, 6000);
+            setTimeout(function() {computer.counter++;}, 4000);
+            continue;
+          }
         }
       }
     }
-
-  }
   res.sendStatus(200);
 })
 
