c22cf247b5e616ddc4bddbf7bf95f100d0be1b35
Peter Aguilar <peter.aguilar2287@gmail.com>
2016-09-19 14:29:06 -0600

    add logic for virus troubleshooting

--- index.js
+++ index.js
@@ -98,21 +98,12 @@ app.post('/webhook/', function (req, res) {
             setTimeout(function() {virus.counter++;}, 4000);
             continue;
           }
-        }else if (response == "bootfixNo"){
-          setTimeout(function() {message.sendTextMessage(sender, "Well sorry this was not helpful.")}, 3000);
-          setTimeout(function() {message.sendTextMessage(sender, "Here are some resources from the internet that may help. \n http://www.pcadvisor.co.uk/how-to/laptop/how-fix-pc-that-wont-boot-3528959/ \n" +
-                                                                  "http://www.howtogeek.com/173828/what-to-do-when-windows-wont-boot/ \n" + "http://www.macworld.co.uk/how-to/mac/10-steps-take-when-your-mac-wont-start-up-or-turn-on-3423817/")}, 4000);
-          setTimeout(function() {message.sendTextMessage(sender, "If you would like help with a new issue just ask. Have a good day.")}, 5000);
-        }else if (response == "intYes"){
-          if(computer.computerVirus[computer.counter] == "end"){
-            setTimeout(function() {message.sendTextMessage(sender, internet.confirmInternetFixed(sender));}, 3000);
-            computer.counter = 0;
-          }else {
-            setTimeout(function() {message.sendTextMessage(sender, internet.internetTroubleshooting[computer.counter]);}, 3000);
-            setTimeout(function() {internet.intConfirmation(sender);}, 6000);
-            setTimeout(function() {computer.counter++;}, 4000);
-            continue;
-          }
+        }else if (response == "cpVirusNo"){
+
+          }else if (response == "mbVirusYes"){
+
+        }else if (response == "mbVirusNo") {
+
         }
       }
     }
