065dcb62f8355487fa3160c1819880c279455ec9
Peter Aguilar <peter.aguilar2287@gmail.com>
2016-09-20 14:00:18 -0600

    fix bug

--- GenericMessage.js
+++ GenericMessage.js
@@ -19,29 +19,5 @@ module.exports = {
         console.log('Error: ', response.body.error);
       }
     })
-  },
-  sendPicture: function(sender) {
-    request({
-      url: 'https://graph.facebook.com/v2.6/me/messages',
-      qs: {access_token:process.env.TOKEN},
-      method: 'POST',
-      json: {
-        recipient: {id:sender},
-        message: {
-          "attachment": {
-            "type": "image",
-            "payload": {
-              "url": "http://pisces.bbystatic.com//BestBuy_US/store/ee/2015/com/pm/nav_desktops_1115.jpg;maxHeight=288;maxWidth=520"
-            }
-          }
-        }
-      }
-    }, function(error, response, body) {
-      if(error) {
-        console.log('Error sending messages: ', error);
-      }else if(response.body.error) {
-        console.log('Error: ', response.body.error);
-      }
-    })
   }
 }
