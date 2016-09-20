cabeb47cb3f8e47800c80cb884fecdcf4b2a8ee9
Peter Aguilar <peter.aguilar2287@gmail.com>
2016-09-20 14:04:43 -0600

    test

--- virus.js
+++ virus.js
@@ -7,33 +7,47 @@ module.exports = {
                   "Next, run the malwarbytes program, and go through the prompts to install it. Refer to this video if you need help: https://www.youtube.com/watch?v=-KBeaAn7hHI", "Once the program is installed, and you have it up and running, click the scan button. This video is helpful if you are lost with this step https://www.youtube.com/watch?v=RjrAceE-W4E",
                   "The program will scan your computer files for any threats. When it is finished it will display any threats that it found. Then it will ask if you want to quarantine the threats and reboot. Once you do this please confirm your issue was fixed.", "end"],
 compVirusConfirmation: function (sender) {
-    let messageData = {
-        "attachment": {
-            "type": "template",
-            "payload": {
-                "template_type": "generic",
-                "elements": [{
-                    "title": "Can you confirm that you completed this step?",
-                    "buttons": [{
-                        "type": "postback",
-                        "title": "Yes",
-                        "payload": "cpVirusYes"
-                    }, {
-                        "type": "postback",
-                        "title": "No",
-                        "payload": "cpVirusNo"
-                    }],
-                }]
-            }
-        }
-    }
+    // let messageData = {
+    //     "attachment": {
+    //         "type": "template",
+    //         "payload": {
+    //             "template_type": "generic",
+    //             "elements": [{
+    //                 "title": "Can you confirm that you completed this step?",
+    //                 "buttons": [{
+    //                     "type": "postback",
+    //                     "title": "Yes",
+    //                     "payload": "cpVirusYes"
+    //                 }, {
+    //                     "type": "postback",
+    //                     "title": "No",
+    //                     "payload": "cpVirusNo"
+    //                 }],
+    //             }]
+    //         }
+    //     }
+    // }
     request({
         url: 'https://graph.facebook.com/v2.6/me/messages',
         qs: {access_token:process.env.TOKEN},
         method: 'POST',
         json: {
             recipient: {id:sender},
-            message: messageData,
+            message: {
+              "text": "Did you finish this step?",
+              "quick_replies": [
+                {
+                  "content_type":"text",
+                  "title":"yes",
+                  "payload":"cpVirusYes"
+                },
+                {
+                  "content_type":"text",
+                  "title":"no",
+                  "payload":"cpVirusNo"
+                }
+              ]
+            }
         }
     }, function(error, response, body) {
         if (error) {
