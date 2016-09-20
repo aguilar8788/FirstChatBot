7c67a5a1e968a90ea6eb9513824ef2786ab21cb7
Peter Aguilar <peter.aguilar2287@gmail.com>
2016-09-20 12:42:24 -0600

    add video support

--- GenericMessage.js
+++ GenericMessage.js
@@ -19,5 +19,30 @@ module.exports = {
         console.log('Error: ', response.body.error);
       }
     })
+  },
+  sendPicture: function(sender, url) {
+    let messageData = { text:text };
+    request({
+      url: 'https://graph.facebook.com/v2.6/me/messages',
+      qs: {access_token:process.env.TOKEN},
+      method: 'POST',
+      json: {
+        recipient: {id:sender},
+        message: {
+          "attachment": {
+            "type": "video",
+            "payload": {
+              "url": url
+            }
+          }
+        }
+      }
+    }, function(error, response, body) {
+      if(error) {
+        console.log('Error sending messages: ', error);
+      }else if(response.body.error) {
+        console.log('Error: ', response.body.error);
+      }
+    })
   }
 }
